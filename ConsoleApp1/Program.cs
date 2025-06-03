469b2db5fbdce60f1841c7470593addbdf08370e
Bruno <User@DESKTOP-HF4OIU3>
2025-06-03 15:34:25 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite numeros de 0 a 9");
+            int x = int.Parse(Console.ReadLine()!);
+            int soma = 0;
+
+       
+            while (x != 0 ) {
+                soma = soma + x;
+                x = int.Parse(Console.ReadLine()!);
+            }   
+            Console.WriteLine("o valor de todo os numeros digitados é: " + soma);
+            Console.ReadLine();
+        }
+    }
+}
